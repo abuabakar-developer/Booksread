17465b8e3677f23ba9e67d97c9169bd0d061c626
abuabakar-developer <157199734+abuabakar-developer@users.noreply.github.com>
2024-08-25 23:17:35 -0700

    third commit

--- src/app/redux/store.tsx
+++ src/app/redux/store.tsx
@@ -12,18 +12,22 @@ import {
 import storage from "redux-persist/lib/storage";
 import cartSlice from "./cartSlice";
 
+// Persist configuration
 const persistConfig = {
     key: "root",
     version: 1,
     storage,
 };
 
+// Combine reducers
 const reducers = combineReducers({
     cart: cartSlice,
 });
 
+// Persisted reducer
 const persistedReducer = persistReducer(persistConfig, reducers);
 
+// Configure store
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
@@ -31,11 +35,9 @@ export const store = configureStore({
             serializableCheck: {
                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             },
-        }),
+        }) as ReturnType<typeof getDefaultMiddleware>, // Cast the middleware type to match expectations
 });
 
 export const persistor = persistStore(store);
-
-
 
 
