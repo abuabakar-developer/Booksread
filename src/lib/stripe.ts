60297e557d7a7a65196c539dabded77aac82827d
abuabakar-developer <157199734+abuabakar-developer@users.noreply.github.com>
2024-08-27 22:08:50 -0700

    first commit

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-08-16',
+  apiVersion: '2024-06-20', // Or a different valid version
 });
 
 export default stripe;
